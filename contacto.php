45646265f470729f1e1b517c48d4851be724122e
Rommel <karlsrommel255@gmail.com>
2023-11-29 17:44:12 -0600

    practicaDePhp007

---
+++ contacto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Gracias por tu mensaje</h1>
+
+<p>Nombre: <?php echo $_POST["nombre"]; ?></p>
+<p>Email: <?php echo $_POST["email"]; ?></p>
+<p>Mensaje: <?php echo $_POST["mensaje"]; ?></p>
+
+<p><a href="index.php">Enviar otro mensaje</a></p>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Formulario de contacto</h1>
+
+<form action="contacto.php" method="post">
+ Nombre:<br>
+ <input type="text" name="nombre"><br>
+ Email:<br>
+ <input type="text" name="email"><br>
+ Mensaje:<br>
+ <textarea name="mensaje"></textarea><br>
+ <input type="submit">
+</form>
+
+</body>
+</html>
